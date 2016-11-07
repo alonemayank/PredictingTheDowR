4df033bf0d50de4a328dcecdad83f8ab58dab8bf
Apurva Tripathi <apurvatripathi@Apurvas-MacBook-Pro.local>
2016-11-07 00:30:05 -0500

    test commit for plots

--- checkpoint3.r
+++ checkpoint3.r
@@ -1,12 +1,17 @@
 library(RSentiment)
 # Read CSV into R
 MyData <- read.csv(file="~/Desktop/PredictingTheDow/stocknews/full-table.csv", header=TRUE, sep=",")
+stockIndices <- read.csv(file="~/Desktop/PredictingTheDow/stocknews/DJIA_table.csv", header=TRUE, sep=",")
+
 summary(MyData)
+summary(stockIndices)
+
+
 sentiment=MyData[,10:34]
 summary(sentiment)
 sentiment[1,1]
 calculate_score(sentiment[1,1])
-colnames(sentiment)
+colname1=colnames(sentiment)
 dim(sentiment)[2]
 mat<-matrix(nrow=1989, ncol=25)
 sentimentResult=mat
@@ -18,4 +23,16 @@ for(i in 1:dim(sentiment)[1]){
     sentimentResult[i,j]=calculate_score(sentiment[i,j])
     }
 }
+
   #print(j)
+colnames(sentimentResult)=colname1
+colnames(sentimentResult)
+dim(sentimentResult)
+mat=sentimentResult
+#mat$Date=paste(stockIndices$Date)
+df=data.frame(mat)
+df$Date=paste(stockIndices$Date)
+par(mfrow=c(1,1))
+par(col="blue")
+plot(stockIndices$Date,stockIndices$Open,type="l",col="red")
+plot(stockIndices$Date, df$Top1, type = "l", col="blue", ylim=c(0,10))
